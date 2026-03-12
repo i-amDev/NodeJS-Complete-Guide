798a531dcb729d31a6942ddabcfedfbac805e478
i-amDev <dev79037@gmail.com>
2026-03-12 15:45:14 +0530

    Added a variable to get the method type from request
    Added one additional if block

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
     // console.log(request.url, request.method, request.headers);
     // process.exit();
     const url = request.url;
+    const method = request.method;
+
     if (url === '/') {
         response.write('<html>');
         response.write('<head><title>Enter Message</title></head>');
@@ -16,6 +19,14 @@ const server = http.createServer((request, response) => {
         response.write('</html>');
         return response.end();
     }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        response.statusCode = 302;
+        response.setHeader('Location', '/');
+        return response.end();
+    }
+    
     response.setHeader('Content-Type', 'text/html');
     response.write('<html>');
     response.write('<head><title>My First Page</title></head>');
