c52cfe0714397c6314ecd7d22f50828a81bb3c69
i-amDev <dev79037@gmail.com>
2026-03-12 17:07:45 +0530

    - Refactored.
    - Moved all the logic to a new file routes.js
    
    - app.js only contain create server logic and calling the method from routes.js
    
    - Learned about exporting as well

--- app.js
+++ app.js
@@ -1,48 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((request, response) => {
-    // console.log(request.url, request.method, request.headers);
-    // process.exit();
-    const url = request.url;
-    const method = request.method;
-
-    if (url === '/') {
-        response.write('<html>');
-        response.write('<head><title>Enter Message</title></head>');
-        response.write('<body>');
-        response.write('<form action="/message" method="POST">');
-        response.write('<input type="text" name="message">');
-        response.write('<button type="submit">Send</button>');
-        response.write('</form>');
-        response.write('</body>');
-        response.write('</html>');
-        return response.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        request.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, error => {
-                response.statusCode = 302;
-                response.setHeader('Location', '/');
-                return response.end();
-            });
-        });
-    }
-
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<html>');
-    response.write('<head><title>My First Page</title></head>');
-    response.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    response.write('</html>');
-    response.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const requestHandler = (request, response) => {
+    const url = request.url;
+    const method = request.method;
+
+    if (url === '/') {
+            response.write('<html>');
+            response.write('<head><title>Enter Message</title></head>');
+            response.write('<body>');
+            response.write('<form action="/message" method="POST">');
+            response.write('<input type="text" name="message">');
+            response.write('<button type="submit">Send</button>');
+            response.write('</form>');
+            response.write('</body>');
+            response.write('</html>');
+            return response.end();
+        }
+    
+        if (url === '/message' && method === 'POST') {
+            const body = [];
+            request.on('data', (chunk) => {
+                console.log(chunk);
+                body.push(chunk);
+            });
+            return request.on('end', () => {
+                const parsedBody = Buffer.concat(body).toString();
+                const message = parsedBody.split('=')[1];
+                fs.writeFile('message.txt', message, error => {
+                    response.statusCode = 302;
+                    response.setHeader('Location', '/');
+                    return response.end();
+                });
+            });
+        }
+    
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html>');
+        response.write('<head><title>My First Page</title></head>');
+        response.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+        response.write('</html>');
+        response.end();
+};
+
+module.exports = requestHandler;
