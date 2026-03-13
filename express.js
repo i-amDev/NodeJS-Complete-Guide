0022f2764a825a1fb272c397f05283c99e06a526
i-amDev <dev79037@gmail.com>
2026-03-13 15:42:42 +0530

    - Learned about middleware

--- express.js
+++ express.js
@@ -1,8 +1,18 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
 const app = express();
+
+app.use((request, response, next) => {
+    console.log('In the middelware!');
+    next(); // Allow the request to continue to the next middleware in line.
+});
+
+app.use('/' ,(request, response, next) => {
+    console.log('In another middelware!');
+    response.send('<h1>Hello from Express!</h1>');
+});
 
 // const server = http.createServer(app);
 
