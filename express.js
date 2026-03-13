c5aa340be8998ed7e0a9be91469bc037d6368696
i-amDev <dev79037@gmail.com>
2026-03-13 15:43:42 +0530

    Added one more middleware for /add-product route

--- express.js
+++ express.js
@@ -9,6 +9,11 @@ app.use((request, response, next) => {
     next(); // Allow the request to continue to the next middleware in line.
 });
 
+app.use('/add-product' ,(request, response, next) => {
+    console.log('In middelware!');
+    response.send('<h1>The "add product" Page!</h1>');
+});
+
 app.use('/' ,(request, response, next) => {
     console.log('In another middelware!');
     response.send('<h1>Hello from Express!</h1>');
