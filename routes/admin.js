ef8fe8706b64fbf95dc1605557b0722cbc0a8370
i-amDev <dev79037@gmail.com>
2026-03-14 15:41:18 +0530

    - Created a new folder to refactor the code
    - Moved the code to admin.js

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product' ,(request, response, next) => {
+    response.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button?</form>');
+});
+
+router.post('/product', (request, response, next) => {
+    console.log(request.body);
+    response.redirect('/');
+});
+
+module.exports = router;
