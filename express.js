68d67c086d8957c7c85a6de2489ee0eaad95e1fc
i-amDev <dev79037@gmail.com>
2026-03-14 12:20:18 +0530

    - Imported the body-parser to parse the body of the incoming requests
    - Added /product endpoint for redirection

--- express.js
+++ express.js
@@ -2,20 +2,30 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((request, response, next) => {
-    console.log('In the middelware!');
-    next(); // Allow the request to continue to the next middleware in line.
-});
+// app.use((request, response, next) => {
+//     // console.log('In the middelware!');
+//     next(); // Allow the request to continue to the next middleware in line.
+// });
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product' ,(request, response, next) => {
-    console.log('In middelware!');
-    response.send('<h1>The "add product" Page!</h1>');
+    // console.log('In middelware!');
+    // response.send('<h1>The "add product" Page!</h1>');
+    response.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button?</form>');
 });
 
+app.use('/product', (request, response, next) => {
+    console.log(request.body);
+    response.redirect('/');
+})
+
 app.use('/' ,(request, response, next) => {
-    console.log('In another middelware!');
+    // console.log('In another middelware!');
     response.send('<h1>Hello from Express!</h1>');
 });
 
